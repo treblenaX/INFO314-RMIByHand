b366652479ae28f9744961791e2250b22db7018e
treblenaX <elbertkcheng@outlook.com>
2023-04-29 19:15:29 -0700

    passed all tests

--- Java/Client.java
+++ Java/Client.java
@@ -1,27 +1,113 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 public class Client {
+    private static Socket socket;
+    private static InputStream is;
+    private static OutputStream os;
 
     /**
      * This method name and parameters must remain as-is
      */
     public static int add(int lhs, int rhs) {
-        return -1;
+        connect();
+
+        RemoteMethod add = new RemoteMethod("add", new Object[] {lhs, rhs});
+
+        writeRequest(add);
+
+        String response = readResponse();
+
+        disconnect();
+
+        return Integer.parseInt(response);
     }
     /**
      * This method name and parameters must remain as-is
      */
-    public static int divide(int num, int denom) {
-        return -1;
+    public static int divide(int num, int denom) throws ArithmeticException {
+        connect();
+
+        RemoteMethod divide = new RemoteMethod("divide", new Object[] {num, denom});
+
+        writeRequest(divide);
+        String response = readResponse();
+
+        if (response.equals("ArithmeticException")) throw new ArithmeticException();
+
+        disconnect();
+
+        return Integer.parseInt(response);
     }
     /**
      * This method name and parameters must remain as-is
      */
     public static String echo(String message) {
-        return "";
+        connect();
+
+        RemoteMethod echo = new RemoteMethod("echo", new Object[] {message});
+
+
+        writeRequest(echo);
+
+        String response = readResponse();
+
+        disconnect();
+
+        return response;
+    }
+
+    private static void connect() {
+        try {
+            socket = new Socket(server, PORT);
+            is = socket.getInputStream();
+            os = socket.getOutputStream();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writeRequest(RemoteMethod rm) {
+        try {
+            // serialize the object
+            ObjectOutputStream oos = new ObjectOutputStream(os);
+            oos.writeObject(rm);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String readResponse() {
+        StringBuilder data = new StringBuilder();
+        try {
+            // readResponse the resposne
+            int character;
+    
+            while ((character = is.read()) != -1) {
+                data.append((char) character);
+            }
+    
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data.toString();
+    }
+
+    private static void disconnect() {
+        try {
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     // Do not modify any code below this line
     // --------------------------------------
-    String server = "localhost";
+    static String server = "localhost";
     public static final int PORT = 10314;
 
     public static void main(String... args) {
@@ -42,11 +128,11 @@ public class Client {
             System.out.print(".");
         }
 
-        if (echo("Hello") == "You said Hello!")
+        if (echo("Hello").equals("You said Hello!"))
             System.out.print(".");
         else
             System.out.print("X");
-        
+
         System.out.println(" Finished");
     }
 }
--- Java/Server.java
+++ Java/Server.java
@@ -1,4 +1,69 @@
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
 public class Server {
+    public static void main(String[] args) {
+        try {
+            ServerSocket server = new ServerSocket(10314);
+            Executor executor = Executors.newFixedThreadPool(5);
+
+            while (true) {
+                Socket socket = server.accept();
+                executor.execute(() -> {
+                    try {
+                        InputStream is = socket.getInputStream();
+                        OutputStream os = socket.getOutputStream();
+    
+                        // read the request
+                        ObjectInputStream ois = new ObjectInputStream(is);
+                        RemoteMethod rm = (RemoteMethod) ois.readObject();
+    
+                        System.out.println("Received request: " + rm.getMethodName() + " " + Arrays.toString(rm.getArgs()));
+                        String methodName = rm.getMethodName();
+                        Object[] parameters = rm.getArgs();
+    
+                        StringBuilder sb = new StringBuilder();
+    
+                        switch (methodName) {
+                            case "add":
+                                sb.append(add((int) parameters[0], (int) parameters[1]));
+                                System.out.println("Result: " + sb.toString());
+                                break;
+                            case "divide":
+                                try {
+                                    sb.append(divide((int) parameters[0], (int) parameters[1]));
+                                } catch (Exception e) {
+                                    sb.append(e.getClass().getSimpleName());
+                                }
+                                System.out.println("Result: " + sb.toString());
+                                break;
+                            case "echo":
+                                sb.append(echo((String) parameters[0]));
+                                System.out.println("Result: " + sb.toString());
+                                break;
+                            default:
+                                throw new Exception("Unknown method: " + methodName);
+                        }
+    
+                        os.write(sb.toString().getBytes());
+                        os.flush();
+                        socket.close();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
     // Do not modify any code below tihs line
     // --------------------------------------
